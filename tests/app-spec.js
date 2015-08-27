4b05cf3907a6ef8aacbcf99ff3e4eae81c8df0c8
Rafael Portela <rafaelportela90@gmail.com>
2015-08-27 16:02:35 -0300

    [@Juraci | @Rafaelportela] mocking the API requests on the end 2 end level

--- tests/app-spec.js
+++ tests/app-spec.js
@@ -1,4 +1,18 @@
 describe('pyxis app', function() {
+
+  beforeEach(function() {
+    var httpBackendMock = function () {
+      var app = angular.module('httpBackendMock', ['ngMockE2E']);
+      app.run(function ($httpBackend) {       
+        var  runs = {runs: [{id: '1', success_percentage: 100}]};
+        $httpBackend.whenGET('http://localhost:5000/runs').respond(runs);
+        $httpBackend.whenGET(/.*/).passThrough();
+      })
+    };
+
+    browser.addMockModule('httpBackendMock', httpBackendMock);
+  });
+
   it('should have a list of runs', function() {
     browser.get('http://localhost:3000/app/index.html');
     var runs = element.all(by.css('.run-item'));
