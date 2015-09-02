226679aedb117cc769bcfcd18ffb2ab259297c6c
Rafael Portela <rafaelportela90@gmail.com>
2015-09-02 16:54:11 -0300

    Unit test for Runs Controller.

--- tests/unit/runSpec.js
+++ tests/unit/runSpec.js
@@ -1,5 +1,25 @@
-describe('something', function() {
-  it('something', function() {
-    expect(3).toBe(3);
+describe('RunController', function() {
+
+  var singleRun = {id: '1234'};
+
+  beforeEach(module('pyxisApp'));
+
+  var $controller, $httpBackend;
+
+  beforeEach(inject(function(_$controller_, _$httpBackend_) {
+    $controller = _$controller_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  it('list runs', function() {
+    var scope = {};
+    $httpBackend.expectGET('http://localhost:5000/runs').respond({
+      runs: [singleRun]
+    });
+
+    var controller = $controller('RunController', {$scope: scope});
+    $httpBackend.flush();
+
+    expect(scope.runs.runs).toEqual([singleRun]);
   });
 });
