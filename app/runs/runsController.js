8dc707e00ba28cf74d5c9fa2318309c4ada3df2a
Rafael Portela <rafaelportela90@gmail.com>
2015-09-03 16:25:01 -0300

    [@dhiana, @rafaelportela] Add support for next page pagination.

--- app/runs/runsController.js
+++ app/runs/runsController.js
@@ -7,4 +7,13 @@ runController.controller('RunController', ['$scope', 'Runs', function($scope, Ru
     $scope.runs = result.runs;
     $scope.pagination = result.meta;
   });
+
+  $scope.next = function() {
+    var limit = $scope.pagination.limit;
+    var offset = $scope.pagination.offset + limit;
+    Runs.query({limit: limit, offset: offset}, function(result) {
+      $scope.runs = result.runs;
+      $scope.pagination = result.meta;
+    });
+  }
 }]);
--- tests/unit/runSpec.js
+++ tests/unit/runSpec.js
@@ -22,4 +22,61 @@ describe('RunController', function() {
 
     expect(scope.runs).toEqual([{id: '1234'}]);
   });
+
+  describe('pagination', function() {
+    describe('the first page is loaded', function() {
+
+      var scope, controller;
+
+      beforeEach(function() {
+        $httpBackend.expectGET(/(.)*\/runs$/).respond(function() {
+          return [200, singleRun, {}];
+        });
+
+        scope = {
+          pagination: {
+            next: "/runs?limit=2&offset=2",
+            offset: 0,
+            limit: 2
+          }
+        }
+
+        controller = $controller('RunController', {$scope: scope});
+      });
+
+      describe('when I click next page', function() {
+
+        var twoRunsWithPagination = {
+          runs: [{id: '1'}, {id: '2'}],
+          meta: {
+            offset: 20,
+            limit: 20
+          }
+        };
+
+        it('loads the second page', function() {
+          $httpBackend.expectGET("http://localhost:5000" + scope.pagination.next).respond(function() {
+            return [200, twoRunsWithPagination, {}];
+          });
+
+          scope.next();
+          $httpBackend.flush();
+
+          expect(scope.runs).toEqual( twoRunsWithPagination.runs );
+        });
+
+        it('updates the pagination info', function() {
+          $httpBackend.expectGET("http://localhost:5000" + scope.pagination.next).respond(function() {
+            return [200, twoRunsWithPagination, {}];
+          });
+
+          expect(scope.pagination.offset).toBe(0);
+          scope.next();
+          $httpBackend.flush();
+
+          expect(scope.pagination.offset).toBe(20);
+        });
+      });
+    });
+  });
 });
